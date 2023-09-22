bd2ace6aeb965bab9ccfa9066aea236261ad4838
Morgan Stenberg <morganstenberg@gmail.com>
2023-09-22 13:05:26 +0000

    add question for testing functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,36 @@
 const startButton = document.getElementById('start-btn')
 const welcomeText = document.getElementById('welcome-text')
 const gameArea = document.getElementById('game-area')
+const nextQuestionButton = document.getElementById('next-btn')
 
 startButton.addEventListener('click', startQuiz)
 
 /** 
  * Function to start the quiz itself, and hide welcome text and start button
+ * calls displayQuestion function
  */
 function startQuiz () {
-welcomeText.classList.add('hide');
-startButton.classList.add('hide');
-gameArea.classList.remove('hide')
+    welcomeText.classList.add('hide');
+    startButton.classList.add('hide');
+    gameArea.classList.remove('hide');
+    nextQuestionButton.classList.remove('hide');
+    displayNexQuestion();
 }
+
+function displayNexQuestion() {
+    
+}
+
+
+const questions = [
+    { 
+        question: "What is the name of Luke Skywalker's home planet?", 
+    
+        answers: [ 
+        { text: "Tatooine", correct: true }, 
+        { text: "Alderaan", correct: false }, 
+        { text: "Naboo", correct: false }, 
+        { text: "Coruscant", correct: false }, 
+         ]
+     },
+    ]    
