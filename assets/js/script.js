b72275d59c1c40b872b2e9a7a295c7a047378304
Morgan Stenberg <morganstenberg@gmail.com>
2023-09-24 11:30:47 +0000

    add more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,32 +161,147 @@ function result () {
 const questions = [
     { 
         question: "What is the name of Luke Skywalker's home planet?", 
-    
         answers: [ 
-        { text: "Tatooine", correct: true }, 
-        { text: "Alderaan", correct: false }, 
-        { text: "Naboo", correct: false }, 
-        { text: "Coruscant", correct: false }, 
+            { text: "Tatooine", correct: true }, 
+            { text: "Alderaan", correct: false }, 
+            { text: "Naboo", correct: false }, 
+            { text: "Coruscant", correct: false }, 
          ]
      },
      { 
         question: "What type of ship does Boba Fett fly in the original trilogy?", 
-    
         answers: [ 
-        { text: "Slave I", correct: true }, 
-        { text: "Imperial Shuttle", correct: false }, 
-        { text: "TIE Fighter", correct: false },
-        { text: "X-wing Starfighter", correct: false },             
+            { text: "Slave I", correct: true }, 
+            { text: "Imperial Shuttle", correct: false }, 
+            { text: "TIE Fighter", correct: false },
+            { text: "X-wing Starfighter", correct: false },             
          ]
      },
      { 
         question: "What is the name of the ancient Sith home world?", 
-    
         answers: [ 
-        { text: "Korriban", correct: true }, 
-        { text: "Dromund Kaas", correct: false }, 
-        { text: "Malachor V", correct: false }, 
-        { text: "Exegol", correct: false }, 
+            { text: "Korriban", correct: true }, 
+            { text: "Dromund Kaas", correct: false }, 
+            { text: "Malachor V", correct: false }, 
+            { text: "Exegol", correct: false }, 
          ]
      },
-    ]    
+    { 
+        question: "What is the name of the ancient and powerful dark side Force users who are sworn enemies of the Jedi?", 
+        answers: [ 
+            { text: "Mandalorians", correct: false }, 
+            { text: "Sith", correct: true }, 
+            { text: "Inquisitors", correct: false },
+            { text: "Nightsisters", correct: false },
+         ] 
+    },
+    { 
+        question: "Which Sith Lord was known as the 'Darth Plagueis the Wise'?", 
+        answers: [ 
+            { text: "Darth Malgus", correct: false }, 
+            { text: "Darth Sidious (Emperor Palpatine)", correct: true }, 
+            { text: "Darth Bane", correct: false }, 
+            { text: "Darth Revan", correct: false }, 
+        ]
+    }, 
+    { 
+        question: "What is the name of the bounty hunter who captured Han Solo in 'Return of the Jedi'?",
+        answers: [ 
+            { text: "Zuckuss", correct: false }, 
+            { text: "Aurra Sing", correct: false }, 
+            { text: "Boushh", correct: true }, 
+            { text: "4-LOM", correct: false }, 
+        ] 
+    }, 
+        
+    { 
+        question: "What type of ship does Boba Fett fly in the original trilogy?", 
+        answers: [ 
+            { text: "Slave I", correct: true }, 
+            { text: "X-wing Starfighter", correct: false }, 
+            { text: "Imperial Shuttle", correct: false }, 
+            { text: "TIE Fighter", correct: false }, 
+        ] 
+    },  
+    { 
+        question: "What is the name of the system where the Rebel Alliance's Echo Base is located in 'The Empire Strikes Back'?", 
+        answers: [ 
+            { text: "Dagobah System", correct: false },
+            { text: "Endor System", correct: false }, 
+            { text: "Hoth System", correct: true }, 
+            { text: "Tatoo System", correct: false }, 
+        ] 
+    },     
+    { 
+        question: "What species is Grand Admiral Thrawn?", 
+        answers: [ 
+            { text: "Rodian", correct: false }, 
+            { text: "Chiss", correct: true }, 
+            { text: "Sullustan", correct: false }, 
+            { text: "Twilek", correct: false }, 
+        ] 
+    },
+    {
+        question: "Who is the leader of the Nightsisters, a clan of Force-sensitive witches on Dathomir?",
+        answers: [
+            { text: "Asajj Ventress", correct: false },
+            { text: "Mother Talzin", correct: true },
+            { text: "Darth Talon", correct: false },
+            { text: "Barris Offee", correct: false },
+        ]
+    },
+    {
+        question: "What is the name of the Wookiee home planet?",
+        answers: [
+            { text: "Kashyyyk", correct: true },
+            { text: "Sullust", correct: false },
+            { text: "Felucia", correct: false },
+            { text: "Kessel", correct: false },
+        ]
+    },
+    {
+        question: "Who is the Sith Lord who took over Mandalore and wielded the Darksaber?",
+        answers: [
+            { text: "Savage Opress", correct: false },
+            { text: "Darth Maul", correct: true },
+            { text: "Darth Malgus", correct: false },
+            { text: "Count Dooku", correct: false },
+        ]
+    },
+    {
+        question: "What is the name of the bounty hunter known for wearing distinctive Mandalorian armor and being a feared enforcer?",
+        answers: [
+            { text: "Jango Fett", correct: false },
+            { text: "Boba Fett", correct: true },
+            { text: "Cade Bane", correct: false },
+            { text: "Zam Wesell", correct: false },
+        ]
+    },
+    {
+        question: "Which Rebel pilot earned the nickname 'Red Leader' during the Battle of Yavin in 'A New Hope'?",
+        answers: [
+            { text: "Wedge Antilles", correct: false },
+            { text: "Garven Dreis", correct: true },
+            { text: "Hobbie Klivian", correct: false },
+            { text: "Biggs Darklighter", correct: false },
+        ]
+    },
+    {
+        question: "What is the name of the infamous bounty hunter who was trained by Jango Fett and played a significant role in the Clone Wars?",
+        answers: [
+            { text: "Aurra Sing", correct: false },
+            { text: "Cad Bane", correct: true },
+            { text: "Embo", correct: false },
+            { text: "Durge", correct: false },
+        ]
+    },
+    {
+        question: "What type of ship does the Millennium Falcon initially appear as?",
+        answers: [
+            { text: "YT-1300 Light Freighter", correct: true },
+            { text: "YT-2000 Light Freighter", correct: false },
+            { text: "YT-2400 Light Freighter", correct: false },
+            { text: "YT-1760 Light Freighter", correct: false },
+        ]
+    },
+];
